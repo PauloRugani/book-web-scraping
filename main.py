170dc4bc9cf0f616c8440ce4b381ee2036174d37
PauloRugani <phrugany12020511@gmail.com>
2025-10-30 20:44:53 -0300

    wip, add: match case bot

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from playwright.sync_api import sync_playwright
-from bots.playwright.main import BookDataExtractor
-import subprocess
+from bots.playwright.main import BookDataExtractorPw
+from bots.selenium.main import BookDataExtractorSel
 
-with sync_playwright() as playwright:
-    data_extractor = BookDataExtractor(playwright,
-                                       'cura quantica and energia and chakras and reiki and lei da atracao and cura natural',
-                                       page_amount=1)
-    data_extractor.run()
+bot = input('bot[selenium, playwright]: ')
+match bot:
+    case 'selenium':
+        data_extractor = BookDataExtractorSel(r'C:\Users\phrug\OneDrive\Documentos\Repositórios\book-web-scraping\bots\selenium\driver\chromedriver.exe',
+                                'cura quantica and energia and chakras and reiki and lei da atracao and cura natural',
+                                page_amount=1)
+        data_extractor.run()  
 
-    
+    case 'playwright':
+        with sync_playwright() as playwright:
+            data_extractor = BookDataExtractorPw(playwright,
+                                            'cura quantica and energia and chakras and reiki and lei da atracao and cura natural',
+                                            page_amount=1)
+            data_extractor.run()
+
+    case _:
+        print('não dei essa opção, mano')
+
