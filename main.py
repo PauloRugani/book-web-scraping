a5bee0ffd286172474b4efff5b8ae85a2f77262c
PauloRugani <phrugany12020511@gmail.com>
2025-10-30 19:17:07 -0300

    wip: main

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from playwright.sync_api import Playwright, sync_playwright
-from bots.playwright.main import run
+from playwright.sync_api import sync_playwright
+from bots.playwright.main import BookDataExtractor
+import subprocess
 
 with sync_playwright() as playwright:
-    run(playwright)
+    data_extractor = BookDataExtractor(playwright,
+                                       'cura quantica and energia and chakras and reiki and lei da atracao and cura natural',
+                                       page_amount=1)
+    data_extractor.run()
+
+    
