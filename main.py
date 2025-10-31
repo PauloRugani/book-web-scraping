20d81e52f5d9713d108e5b856c749630b1f62730
PauloRugani <phrugany12020511@gmail.com>
2025-10-31 16:43:31 -0300

    wip: google scraper

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 from playwright.sync_api import sync_playwright
-from bots.playwright.main import BookDataExtractorPw
-from bots.selenium.main import BookDataExtractorSel
 
+from amazon.bots.playwright.main import AmazonBookExtractorPw
+from amazon.bots.selenium.main import AmazonBookExtractorSel
+
+SEARCH_STRING = 'cura quantica and energia and chakras and reiki and lei da atracao and cura natural'
+PAGE_AMOUNT = 1
+
+website = input('website[amazon, google]')
 bot = input('bot[selenium, playwright]: ')
-match bot:
-    case 'selenium':
-        data_extractor = BookDataExtractorSel(r'bots\selenium\driver\chromedriver.exe',
-                                'cura quantica and energia and chakras and reiki and lei da atracao and cura natural',
-                                page_amount=1)
-        data_extractor.run()  
 
-    case 'playwright':
+if website.lower() == 'amazon':
+    if bot.lower() == 'selenium':
+        book_scraper = AmazonBookExtractorSel(SEARCH_STRING, PAGE_AMOUNT)
+        book_scraper.run()
+    elif bot.lower() == 'playwright':
         with sync_playwright() as playwright:
-            data_extractor = BookDataExtractorPw(playwright,
-                                            'cura quantica and energia and chakras and reiki and lei da atracao and cura natural',
-                                            page_amount=1)
-            data_extractor.run()
+            book_scraper = AmazonBookExtractorPw(playwright, SEARCH_STRING, PAGE_AMOUNT)
+            book_scraper.run()
+    else:
+        print('Não dei essa opção de site, mano')
+                   
+elif website.lower() == 'google':
+    if bot.lower() == 'selenium':
+        book_scraper = AmazonBookExtractorSel(SEARCH_STRING, PAGE_AMOUNT)
+        book_scraper.run()
+    elif bot.lower() == 'playwright':
+        with sync_playwright() as playwright:
+            book_scraper = AmazonBookExtractorPw(playwright, SEARCH_STRING, PAGE_AMOUNT)
+            book_scraper.run()
+    else:
+        print('Não dei essa opção de robô, mano')
 
-    case _:
-        print('não dei essa opção, mano')
-
+else:
+    print('Não dei essa opção de site, mano')
+                    
+                    
