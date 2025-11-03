d2a340b323e16a4dfb4c868db109c2ae4c1dbc0c
PauloRugani <phrugany12020511@gmail.com>
2025-11-03 16:40:30 -0300

    add: init import file

---
+++ amazon/__init__.py
@@ -0,0 +1,4 @@
+from amazon.constants.main import Constants
+from amazon.bots.playwright.constants.main import PlaywrightConstants
+from amazon.bots.selenium.constants.main import SeleniumConstants
+from amazon.utils.SearchStringToUrl import SearchStringToUrl
